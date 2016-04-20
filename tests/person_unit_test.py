7812e1cd3a2e1541750b077fbd00caa7d8a4e2d4
andela-ydaniju <yusuf.daniju@andela.com>
2016-04-20 12:42:17 +0100

    refactor tests to green

--- tests/person_unit_test.py
+++ tests/person_unit_test.py
@@ -13,7 +13,12 @@ class TestPerson(unittest.TestCase):
 
     def test_person_is_initialized_without_office_name(self):
         man = Person("Yusuf")
-        self.assertEqual(None, man.office_name)
+        self.assertIsNone(man.office_name)
+
+    def test_person_without_office_can_be_assigned_one(self):
+        man = Person("Yusuf")
+        man.assign_office("Carat")
+        self.assertEqual("Carat", man.office_name)
 
 if __name__ == '__main__':
     unittest.main()
