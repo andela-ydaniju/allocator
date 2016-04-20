bb9f966f259905283f02b796246a2273cc0ea27a
andela-ydaniju <yusuf.daniju@andela.com>
2016-04-20 12:30:19 +0100

    finish modelling person

--- tests/person_unit_test.py
+++ tests/person_unit_test.py
@@ -4,12 +4,16 @@ from .context import Person
 class TestPerson(unittest.TestCase):
  
     def test_person_is_initialized_with_name(self):
-        man = Person("Yusuf", "Fellow")
+        man = Person("Yusuf", "Moon")
         self.assertEqual("Yusuf", man.name)
 
-    def test_person_is_initialized_with_status(self):
-        man = Person("Yusuf", "Fellow")
-        self.assertEqual("Fellow", man.status)
+    def test_person_is_initialized_with_office_name(self):
+        man = Person("Yusuf", "Moon")
+        self.assertEqual("Moon", man.office_name)
+
+    def test_person_is_initialized_without_office_name(self):
+        man = Person("Yusuf")
+        self.assertEqual(None, man.office_name)
 
 if __name__ == '__main__':
     unittest.main()
